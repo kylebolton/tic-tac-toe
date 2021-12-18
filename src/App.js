9f18deb9c26915cb590924aba87cf64caea60c1d
Kyle Bolton <hello@kylebolton.me>
2021-12-18 17:39:37 +0000

    Format/prettier

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import React from 'react';
-import Table from './Components/Table'
-import styled from 'styled-components';
-import { createGlobalStyle } from 'styled-components';
+import React from "react";
+import Table from "./Components/Table";
+import styled from "styled-components";
+import { createGlobalStyle } from "styled-components";
 
 function App() {
   return (
-      <React.Fragment>
-          <GlobalStyle/>
-          <MainContainer>
-              <Table />
-          </MainContainer>
-      </React.Fragment>
-
+    <React.Fragment>
+      <GlobalStyle />
+      <MainContainer>
+        <Table />
+      </MainContainer>
+    </React.Fragment>
   );
 }
 
 export default App;
 
-const MainContainer = styled('div')`
+const MainContainer = styled("div")`
   text-align: center;
   display: flex;
   justify-content: center;
   align-items: center;
-`
+`;
 
 const GlobalStyle = createGlobalStyle`
   @-webkit-keyframes bg-scrolling-reverse {
@@ -75,10 +74,9 @@ const GlobalStyle = createGlobalStyle`
 
   body {
     margin-top: 13.5rem;
-    color: #797979;
     text-align: center;
     background: url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAIAAACRXR/mAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAIGNIUk0AAHolAACAgwAA+f8AAIDpAAB1MAAA6mAAADqYAAAXb5JfxUYAAABnSURBVHja7M5RDYAwDEXRDgmvEocnlrQS2SwUFST9uEfBGWs9c97nbGtDcquqiKhOImLs/UpuzVzWEi1atGjRokWLFi1atGjRokWLFi1atGjRokWLFi1af7Ukz8xWp8z8AAAA//8DAJ4LoEAAlL1nAAAAAElFTkSuQmCC") repeat 0 0;
     animation: bg-scrolling-reverse 0.92s infinite;
     animation-timing-function: linear;
   }
-`
+`;
--- src/Components/Square.js
+++ src/Components/Square.js
@@ -1,43 +1,60 @@
-import {ReactComponent as Circle} from "../img/circle.svg";
-import {ReactComponent as X} from '../img/x.svg';
-import React from 'react';
-import styled from 'styled-components';
+import { ReactComponent as Circle } from "../img/circle.svg";
+import { ReactComponent as X } from "../img/x.svg";
+import React from "react";
+import styled from "styled-components";
 
 const Square = (props) => {
-    const number = props.number
-    const [last, setLast] = props.last
-    const [table, setTable] = props.table
-    const winner = props.winner
+  const number = props.number;
+  const [last, setLast] = props.last;
+  const [table, setTable] = props.table;
+  const winner = props.winner;
 
-    const Image = props => {
-        if (props.tableN === 5) {
-            return <X fill={winner.indexOf(number) !== -1? '#64d369':"#6e6e6e"} width="100%" height="100%"/>
-        } else if ( props.tableN === 3 ){
-            return <Circle fill={winner.indexOf(number) !== -1? '#64d369':"#6e6e6e"} width="100%" height="100%"/>
-        } else {
-         return <img/>
-        }
+  const Image = (props) => {
+    if (props.tableN === 5) {
+      return (
+        <X
+          fill={winner.indexOf(number) !== -1 ? "#64d369" : "#6e6e6e"}
+          width="100%"
+          height="100%"
+        />
+      );
+    } else if (props.tableN === 3) {
+      return (
+        <Circle
+          fill={winner.indexOf(number) !== -1 ? "#64d369" : "#6e6e6e"}
+          width="100%"
+          height="100%"
+        />
+      );
+    } else {
+      return <img />;
     }
-    
-    return (
-        <Block onClick={() => {
-            if (table[number] === 0 && winner.length === 0) {
-            setTable(( () => {
-                let newTable = [...table];
-                let newLast = last === 3? 5:3
-                newTable[number] = newLast
-                setLast(newLast)
-                return newTable
-            })())}
-            }}>
-            <Image tableN={table[number]}/> 
-        </Block>
-    )
-}
+  };
+
+  return (
+    <Block
+      onClick={() => {
+        if (table[number] === 0 && winner.length === 0) {
+          setTable(
+            (() => {
+              let newTable = [...table];
+              let newLast = last === 3 ? 5 : 3;
+              newTable[number] = newLast;
+              setLast(newLast);
+              return newTable;
+            })()
+          );
+        }
+      }}
+    >
+      <Image tableN={table[number]} />
+    </Block>
+  );
+};
 
 export default Square;
 
-const Block = styled('div')`
+const Block = styled("div")`
   padding: 9px;
   border: solid;
   border-width: 0.5px;
@@ -45,4 +62,4 @@ const Block = styled('div')`
   border-color: #d1d1d1;
   width: 5em;
   height: 5em;
-`
+`;
