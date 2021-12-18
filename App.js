9705f2e3aa16668874cf1a516ed8409bef0bb8bc
Kyle Bolton <hello@kylebolton.me>
2021-12-18 18:12:42 +0000

    Fixed height/removed margin

---
+++ App.js
@@ -0,0 +1,47 @@
+import React from "react";
+import Table from "./Components/Table";
+import styled from "styled-components";
+import { createGlobalStyle } from "styled-components";
+
+function App() {
+  return (
+    <React.Fragment>
+      <GlobalStyle />
+      <MainContainer>
+        <Table />
+      </MainContainer>
+    </React.Fragment>
+  );
+}
+
+export default App;
+
+const MainContainer = styled("div")`
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const GlobalStyle = createGlobalStyle`
+  @keyframes bg-scrolling-reverse {
+    100% {
+      background-position: 50px 50px;
+    }
+  }
+
+  @keyframes bg-scrolling {
+    0% {
+      background-position: 50px 50px;
+    }
+  }
+
+  body {
+    height: 100vh;
+    text-align: center;
+    background: url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAIAAACRXR/mAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAIGNIUk0AAHolAACAgwAA+f8AAIDpAAB1MAAA6mAAADqYAAAXb5JfxUYAAABnSURBVHja7M5RDYAwDEXRDgmvEocnlrQS2SwUFST9uEfBGWs9c97nbGtDcquqiKhOImLs/UpuzVzWEi1atGjRokWLFi1atGjRokWLFi1atGjRokWLFi1af7Ukz8xWp8z8AAAA//8DAJ4LoEAAlL1nAAAAAElFTkSuQmCC") repeat 0 0;
+    animation: bg-scrolling-reverse 0.92s infinite;
+    animation-timing-function: linear;
+  }
+`;
