bb002c6caa39ff47be45a02ce777a24f03da91bd
Kyle Bolton <hello@kylebolton.me>
2021-12-18 17:18:54 +0000

    Added new animation background

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Table from './Components/Table'
 import styled from 'styled-components';
+import { createGlobalStyle } from 'styled-components';
 
 function App() {
   return (
-      <MainContainer>
-          <Table />
-      </MainContainer>
+      <React.Fragment>
+          <GlobalStyle/>
+          <MainContainer>
+              <Table />
+          </MainContainer>
+      </React.Fragment>
 
   );
 }
@@ -15,8 +19,66 @@ export default App;
 
 const MainContainer = styled('div')`
   text-align: center;
-  height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
+`
+
+const GlobalStyle = createGlobalStyle`
+  @-webkit-keyframes bg-scrolling-reverse {
+    100% {
+      background-position: 50px 50px;
+    }
+  }
+
+  @-moz-keyframes bg-scrolling-reverse {
+    100% {
+      background-position: 50px 50px;
+    }
+  }
+
+  @-o-keyframes bg-scrolling-reverse {
+    100% {
+      background-position: 50px 50px;
+    }
+  }
+
+  @keyframes bg-scrolling-reverse {
+    100% {
+      background-position: 50px 50px;
+    }
+  }
+
+  @-webkit-keyframes bg-scrolling {
+    0% {
+      background-position: 50px 50px;
+    }
+  }
+
+  @-moz-keyframes bg-scrolling {
+    0% {
+      background-position: 50px 50px;
+    }
+  }
+
+  @-o-keyframes bg-scrolling {
+    0% {
+      background-position: 50px 50px;
+    }
+  }
+
+  @keyframes bg-scrolling {
+    0% {
+      background-position: 50px 50px;
+    }
+  }
+
+  body {
+    margin-top: 13.5rem;
+    color: #797979;
+    text-align: center;
+    background: url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAIAAACRXR/mAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAIGNIUk0AAHolAACAgwAA+f8AAIDpAAB1MAAA6mAAADqYAAAXb5JfxUYAAABnSURBVHja7M5RDYAwDEXRDgmvEocnlrQS2SwUFST9uEfBGWs9c97nbGtDcquqiKhOImLs/UpuzVzWEi1atGjRokWLFi1atGjRokWLFi1atGjRokWLFi1af7Ukz8xWp8z8AAAA//8DAJ4LoEAAlL1nAAAAAElFTkSuQmCC") repeat 0 0;
+    animation: bg-scrolling-reverse 0.92s infinite;
+    animation-timing-function: linear;
+  }
 `
